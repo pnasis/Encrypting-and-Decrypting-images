617f4ff72e19cbc584d5834519f4b28536fa42ac
Prodromos Nasis <79412734+pnasis@users.noreply.github.com>
2022-02-16 17:12:41 +0200

    Add files via upload

---
+++ random_password_generator.py
@@ -0,0 +1,12 @@
+import random 
+ 
+# The sets we are using to generate the password 
+lower = "abcdefghijklmnopqrstuvwxyz" 
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+numbers = "0123456789" 
+symbols = "[]*/-_$#@!%" 
+ 
+all = lower + upper + numbers + symbols  #Universal Set 
+length = 16   #You can change the length of the password. 
+password = "".join(random.sample(all, length)) 
+print(password)
